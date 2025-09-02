6374cca1cdfc9562fe048ced0a353d03a6daa66f
Sergio Paulo Perez <giolo.perez@lawadvisor.com>
2025-09-02 13:13:36 +0800

    Add user email in comment response

--- lib/giolotrello_api_web/controllers/comment_json.ex
+++ lib/giolotrello_api_web/controllers/comment_json.ex
@@ -1,5 +1,6 @@
 defmodule GiolotrelloApiWeb.CommentJSON do
   alias GiolotrelloApi.Comments.Comment
+  alias GiolotrelloApi.Users.User
 
   def index(%{comments: comments}) do
     %{data: for(comment <- comments, do: data(comment))}
@@ -11,11 +12,14 @@ defmodule GiolotrelloApiWeb.CommentJSON do
 
   defp data(%Comment{} = comment) do
     %{
-      id: comment.id,
-      body: comment.body,
-      user_id: comment.user_id,
-      email: comment.user.email,
-      task_id: comment.task_id
+      "id" => comment.id,
+      "body" => comment.body,
+      "user_id" => comment.user_id,
+      "task_id" => comment.task_id,
+      "email" => get_user_email(comment.user)
     }
   end
+
+  defp get_user_email(%User{email: email}), do: email
+  defp get_user_email(_), do: nil
 end
