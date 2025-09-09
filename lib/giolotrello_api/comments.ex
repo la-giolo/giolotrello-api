923da63aa82fc28104438ec2aadf31c8e49ca848
Sergio Paulo Perez <giolo.perez@lawadvisor.com>
2025-09-09 17:53:47 +0800

    Add email as a response in create comment

--- lib/giolotrello_api/comments.ex
+++ lib/giolotrello_api/comments.ex
@@ -23,11 +23,25 @@ defmodule GiolotrelloApi.Comments do
   @spec get_comment!(any()) :: any()
   def get_comment!(id), do: Repo.get!(Comment, id)
 
+  def list_comments(task_id) do
+    Comment
+    |> where(task_id: ^task_id)
+    |> preload(:user)
+    |> Repo.all()
+  end
+
   def create_comment(attrs) do
     %Comment{}
     |> Comment.create_changeset(attrs)
     |> Repo.insert()
+    |> case do
+      {:ok, comment} ->
+        {:ok, Repo.preload(comment, :user)}
+      error ->
+        error
+    end
   end
+
 
   def update_comment(%Comment{} = comment, attrs) do
     comment
